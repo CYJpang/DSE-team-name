ade648da27c3bcbe8e13d54b03bbc42bae15129a
Replaceable-Chad <s4003969@vuw.leidenuniv.nl>
2025-10-23 14:31:47 +0200

    Add hello_world.py with main function and input
    I think this addition will cause a lot of happiness and team morale

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+def main():
+  hello_world = 'hello_world'
+  
+  command = input('What do you want to say?')
+  
+  if command == hello_world:
+    print('HELL YEAH')
+  else:
+    print('you are lying....')
+  
+  print('bye_bye')
+  return None
+
+if __name__ == '__main__':
+  main()
+  
